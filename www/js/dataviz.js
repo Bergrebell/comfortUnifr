723b4cfc62b1d39126012d605b10aef65f7b4b80
Roman Kuepper <rrkuepper@gmail.com>
2015-09-22 10:50:29 +0200

    noise data visualization working but scale not yet adapted

--- www/js/dataviz.js
+++ www/js/dataviz.js
@@ -55,21 +55,31 @@ $(document).ready(function() {
         // connect to the existin localDB database
         var db = window.openDatabase("localDB", "1.0", "Local DB", 10000000);
         db.transaction(runSelect, error);
+        
+        // create empty data arrays
+            var noiseUarray = [];
+            var noiseSarray = [];
 
 
         function runSelect(t){
-            t.executeSql('SELECT * FROM comfort LIMIT 20 OFFSET (SELECT COUNT(*) FROM comfort)-20;', [], success, error);
+            t.executeSql('SELECT * FROM comfort LIMIT 10 OFFSET (SELECT COUNT(*) FROM comfort)-10;', [], success, error);
         }
         function success(t, results){
             console.log('Number of rows: '+results.rows.length);
             
-            for(var i = 0; i < 20; i++){
+            for(var i = 0; i < 10; i++){
                 console.log('Row: '+i);
                 console.log('Noise: '+results.rows.item(i).noiseU);
-                console.log('Light: '+results.rows.item(i).lightU);
                 console.log('NoiseS: '+results.rows.item(i).noiseS);
+                noiseUarray.push(results.rows.item(i).noiseU);
+                noiseSarray.push(results.rows.item(i).noiseS);
+                
+                console.log('Light: '+results.rows.item(i).lightU);
                 console.log('LightS: '+results.rows.item(i).lightS);
             }
+                console.log("Senserarray: "+ noiseSarray);
+                console.log("Userarray: " + noiseUarray);
+                var noiseLineChart = new Chart(ctx).Line(noiseData, options);
         }
         function error(err){
             console.log('There was an error processing the SQL: '+err);
@@ -82,72 +92,71 @@ $(document).ready(function() {
 
 
 
+        var ctx = $("#noiseCanvas").get(0).getContext("2d");
 
-
-    var ctx = $("#noiseCanvas").get(0).getContext("2d");
-
-    var noiseData = {
-        labels: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
-
-
-
-    var noiseLineChart = new Chart(ctx).Line(noiseData, options);
-
-
-    var ctx2 = $("#lightCanvas").get(0).getContext("2d");
-
-    var lightData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [65, 59, 80, 81, 56, 55, 40]
-            },
-            {
-                label: "My Second dataset",
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(151,187,205,1)",
-                data: [28, 48, 40, 19, 86, 27, 90]
-            }
-        ]
-    };
+        var noiseData = {
+            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: noiseUarray
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: noiseSarray
+                }
+            ]
+        };
 
 
 
-    var lightLineChart = new Chart(ctx2).Line(lightData, options);
+        
+
+
+        var ctx2 = $("#lightCanvas").get(0).getContext("2d");
+
+        var lightData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+
+
+
+        var lightLineChart = new Chart(ctx2).Line(lightData, options);
+    
 
 });
 
